69d124bfca368ba498a84beef726493b544639e4
Alexander Friedrich <johnendzsocial@icloud.com>
2020-03-14 19:50:34 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// mudar imagem da tela inicial | change image home
+setTimeout(function() {
+    document.querySelector(".dropbox-logo__type").src="https://i.imgur.com/vMzrUH8.png";
+}, 1000);
+setTimeout(function() {
+    document.querySelector(".dropbox-logo__type").style.display = "block";
+}, 2000);
+
+setTimeout(function() {
+    document.querySelector(".DropboxLogo--wordmark").src="https://i.imgur.com/vMzrUH8.png";
+}, 1000);
+setTimeout(function() {
+    document.querySelector(".DropboxLogo--wordmark").style.display = "block";
+}, 2000);
